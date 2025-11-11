f0c231c7942b159e2bc8f332e283517b67216873
LIZI WANG <liziwang.uk@gmail.com>
2025-11-11 09:57:37 +0000

    Update server.js

--- Website/server/server.js
+++ Website/server/server.js
@@ -23,4 +23,4 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '../client/index.html'))
 );
 
-app.listen(3000, () => console.log('http://localhost:3000'));
+app.listen(3000, "0.0.0.0");
